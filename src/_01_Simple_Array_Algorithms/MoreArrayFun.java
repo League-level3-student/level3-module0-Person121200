c6f573c797238c630bb407f80c7b28ce60fada2a
league <league@imac-28.attlocal.net>
2025-02-23 11:26:42 -0800

    Working on 2d arrays

--- src/_01_Simple_Array_Algorithms/MoreArrayFun.java
+++ src/_01_Simple_Array_Algorithms/MoreArrayFun.java
@@ -3,29 +3,63 @@ package _01_Simple_Array_Algorithms;
 import java.util.Random;
 
 public class MoreArrayFun {
+	
     //1. Create a main method to test the other methods you write.
 	public static void main(String[] args) {
-		
+		String[] array = {"hi", "hi", "hi", "GONE", "skibidi", "ohio", "aw hell nah", "omg", "hi"};
+		Random(array);
 	}
 
 
     //2. Write a method that takes an array of Strings and prints all the Strings in the array.
 	public static String Strings(String[] array) {
+		for(int i = 0; i< array.length; i++) {
+			System.out.println(array[i]);
+		}
+		return "";
 		
-		return "Strings";
 	}
 
 
     //3. Write a method that takes an array of Strings and prints all the Strings in the array
     //   in reverse order.
-
-
+	public static String Reverse(String[] array) {
+		for(int i = array.length-1; i>=0; i--) {
+			System.out.println(array[i]);
+		}
+		return "";
+		
+	}
 
     //4. Write a method that takes an array of Strings and prints every other String in the array.
-
+	public static String EveryOtherString(String[] array) {
+		for(int i = 0; i< array.length; i++) {
+			if(i %2==1) {
+				System.out.println(array[i]);
+			}
+		}
+		return "";
+		
+	}
 
     //5. Write a method that takes an array of Strings and prints all the Strings in the array
     //   in a completely random order. Almost every run of the program should result in a different order.
+	public static void Random(String[] array) {
+		Random rand = new Random();
+		 for (int i = array.length - 1; i > 0; i--) {
+	            int randomIndex = rand.nextInt(i + 1);
+	            String temp = array[i];
+	            array[i] = array[randomIndex];
+	            array[randomIndex] = temp;
+	        }
+		 for (String str : array) {
+	            System.out.println(str);
 
-
+		
+		
+		
+	}
+	}
 }
+
+
--- src/_03_Intro_to_2D_Arrays/_01_IntroTo2DArrays.java
+++ src/_03_Intro_to_2D_Arrays/_01_IntroTo2DArrays.java
@@ -1,13 +1,22 @@
 package _03_Intro_to_2D_Arrays;
 
+import java.util.Random;
+
 public class _01_IntroTo2DArrays {
 
     public static void main(String[] args) {
         // 1. declare a 2D array of integers with 3, 1D arrays that each hold
         //    3 integers
-
+    	int [][] array2d = new int [3][3];
         // 2. use two for loops to set all the elements in the 2D array to
         //    a different integer
+    	Random rand = new Random();
+    	for(int i = 0; i<array2d.length; i++) {
+    		for(int j = 0; j<array2d[i].length; j++) {
+    			array2d[i][j] = rand.nextInt(100);
+    		}
+    		System.out.println(array2d[1][2]);
+    	}
         //    BE SURE TO USE THE ARRAY'S length VARIABLE
         
         // 3. print the third element from the second 1D array (2DArray[1][2])
